68997840ff74c75e2bf2b82105b3c12b23821889
EdenIsakka <julianig84@hotmail.com>
2022-10-04 16:38:03 -0500

    Segundo fetch agragdo

--- src/components/company.js
+++ src/components/company.js
@@ -1,55 +1,71 @@
-import { useEffect, useState } from "react"
-import Card from 'react-bootstrap/Card';
-
+import { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Station from "./station";
 
 function Companys() {
+  const [networks, setNetworks] = useState([]);
+  const [company, setcompany] = useState([]);
+  const [show, setShow] = useState(null);
 
+  var url = "http://api.citybik.es/v2/networks";
 
-    const[networks, setNetworks] = useState([])
+  const consumirAPI = async () => {
+    const response = await fetch(url, { method: "GET" })
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
 
-    var url = "http://api.citybik.es/v2/networks"
-    
+    setNetworks(response.networks);
+  };
 
+  const consumirAPI2 = async (href) => {
+    let newUrl = "http://api.citybik.es" + href;
+    const response = await fetch(newUrl, { method: "GET" })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
 
-    const consumirAPI = async() => {
-       const response = await fetch(url, { method: 'GET' }).then(response => response.json())
-       .catch(error => console.log(error))
+    setcompany(response.network);
+    setShow(true);
+  };
 
+  useEffect(() => {
+    consumirAPI();
+  }, []);
 
-       setNetworks(response.networks)
-    }
-
-    useEffect(() => {
-        consumirAPI()
-    }, [])
-
-    console.log(networks)
-
-
-    return(
-        <div className="div_container">
-            <h1>Empresas</h1>
-            {
-                networks.map((network) => {
-                    return(
-                        <div className="card" key={network.id}>
-                            
-                        <Card className="cards_style" border="primary">
-                            <Card.Body>
-                            <Card.Title>Nombre de la compañia: {network.company}</Card.Title>
-                            <Card.Text>Nombre de la red: {network.name}</Card.Text>
-                            <Card.Text>Localicacion del país: {network.location.country}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                                
-                            
-                            
-                            
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+  return (
+    <div className="div_container">
+      <h1>Empresas</h1>
+      {show ? 
+      <Station
+      show={show}
+      data={company}
+      handleClose={() => setShow(false)}
+    ></Station>
+      : null}
+      {networks.map((network) => {
+        return (
+          <div className="card" key={network.id}>
+            <Card className="cards_style" border="primary">
+              <Card.Body>
+                <Card.Title>
+                  Nombre de la compañia: {network.company}
+                </Card.Title>
+                <Card.Text>Nombre de la red: {network.name}</Card.Text>
+                <Card.Text>
+                  Localicacion del país: {network.location.country}
+                </Card.Text>
+              </Card.Body>
+              <Button
+                variant="primary"
+                onClick={() => consumirAPI2(network.href)}
+              >
+                Launch
+              </Button>
+            </Card>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
-export default Companys
+export default Companys;
--- src/components/station.js
+++ src/components/station.js
@@ -1,25 +1,42 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Card from "react-bootstrap/Card";
 
-
-
-
-function Station () {
-
-
-
-
-
-
-
-
-
-    return(
-
-        <div>
-
-        </div>
-
-
-
-
-    )
+function Station(props) {
+  if(!props.data.stations) console.log("Cargando");
+  let stations = props.data.stations
+  console.log(stations);
+  return (
+    <>
+      <Offcanvas show={props.show} onHide={props.handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>{props.data.name}</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div>
+          {stations.map(station => {
+        return (
+          <div key={station.id}>
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  Nombre de la estacion: {station.name}
+                </Card.Title>
+                <Card.Text>Ultima actualizacion: {station.timestamp}</Card.Text>
+                <Card.Text>Bicicletas libres: {station.free_bikes}</Card.Text>
+                <Card.Text>Espacios libres: {station.free_slots}</Card.Text>
+              </Card.Body>
+              
+            </Card>
+          </div>
+        );
+      })}
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
 }
+
+export default Station;
